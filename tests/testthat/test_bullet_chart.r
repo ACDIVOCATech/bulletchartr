cffba094ca4b3c0ae15cb198b5bffba3b03e2e28
Ryo-N7 <ryonakagawara@gmail.com>
2019-06-27 18:55:41 +0900

    add some tests, need to think about WHERE to put data files for testing...

--- tests/testthat/test_bullet_chart.r
+++ tests/testthat/test_bullet_chart.r
@@ -2,13 +2,36 @@ context("bulletchart")
 
 #testthat::test_that("small argument must be TRUE or FALSE",
 #                    expect_error(bullet_chart(dataframe = df, small = 2), "small must be T/F"))
-
+load("inst/data/df.Rda")
 
 ## Ensure both dataframe and file not provided
-testthat::test_that("Only one dataset inputted: dataframe OR file_name - not both",
-                    testthat::expect_error(
+testthat::test_that("Only one dataset inputted: dataframe OR file_name - not both", {
 
-                      bullet_chart(dataframe = df, file_name = "inst/data/Indicators_Targets.xlsx")
+  expect_error(bullet_chart(dataframe = df, file_name = "inst/data/Indicators_Targets.xlsx"))
 
-                      )
-                    )
+  expect_error(bullet_chart(dataframe = df), NA)
+
+  expect_error(bullet_chart(file_name = "inst/data/Indicators_Targets.xlsx"), NA)
+
+})
+
+
+## Expect inputs
+testthat::test_that("correct arguments used", {
+
+  file_name <- "inst/data/Indicators_Targets.xlsx"
+  checkmate::expect_data_frame(df)
+  checkmate::expect_character(file_name)
+
+})
+
+
+## Expect outputs
+testthat::test_that("correct outputs", {
+
+  ## when change to static vs. interactive >> needs to change to include dif arg versions
+  expect_equal(class(bullet_chart(dataframe = df)), c("girafe", "htmlwidget"))
+  expect_equal(class(bullet_chart_symbols(dataframe = df)), c("girafe", "htmlwidget"))
+  expect_equal(class(bullet_chart_vline(dataframe = df)), c("girafe", "htmlwidget"))
+  expect_equal(class(bullet_chart_wide(dataframe = df)), c("girafe", "htmlwidget"))
+})
--- tests/testthat/test_extra_field_calculator.r
+++ tests/testthat/test_extra_field_calculator.r
@@ -11,12 +11,12 @@ context("extra_field_calculator")
 
 
 # ## Inputs
-# fileName="Indicators_Targets.com"   # NOT excel file
-# fileName="data/Indicators_Targets.xlsx"  # excel file
-# for_year = 2018 ## Specify Year the analysis represents
-# FY = TRUE       ## Is this a fiscal year? (as opposed to calendar year)
-# project_start_date <- "2016/03/01"   # as string! %Y/%D/%M
-#
-# ##
-# dataframe <- extra_field_calculator("data/Indicators_Targets.xlsx", for_year = 2018, FY = TRUE, project_start_date = project_start_date)
-# df <- extra_field_calculator(fileName, for_year = 2018, FY = TRUE, project_start_date = project_start_date)
+fileName = "Indicators_Targets.com"   # NOT excel file
+fileName = here::here("inst/data/Indicators_Targets.xlsx")  # excel file
+for_year = 2018 ## Specify Year the analysis represents
+FY = TRUE       ## Is this a fiscal year? (as opposed to calendar year)
+project_start_date <- "2016/03/01"   # as string! %Y/%D/%M
+
+##
+dataframe <- extra_field_calculator("data/Indicators_Targets.xlsx", for_year = 2018)
+df <- extra_field_calculator(fileName, for_year = 2018)
