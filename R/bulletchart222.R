bee62c67f15c9950ec6d1c7d5b927da609fc7c40
Ryo-N7 <ryonakagawara@gmail.com>
2019-10-10 19:10:41 +0900

    create -regular- version, call ACDIVOCA as time-comparison version, references #22

--- R/bulletchart222.R
+++ R/bulletchart222.R
@@ -1,81 +1,105 @@
-# bcdata <- tibble::tibble(
-#   variable = c("Revenue", "Order Size",
-#                "New Customers", "Satisfaction"),
-#   target = c(325, 525, 975, 4.5),
-#   value = c(365, 310, 1050, 4),
-#   bad = c(220, 430, 600, 2.5),
-#   good = c(240, 480, 770, 3.25),
-#   great = c(300, 505, 1100, 4.5)
-# )
-#
-# bcdata
-#
-#
-# bcdata %>%
-#   #filter(variable == "Revenue") %>%
-#   pivot_longer(-c(variable, target),
-#                names_to = "allvals", values_to = "value") %>%
-#   ggplot() +
-#   geom_col(aes(x = variable, y = value, fill = allvals)) +
-#   scale_y_continuous(limits = c(0, NA)) +
-#   coord_flip() +
-#   facet_wrap(~variable, scales = "free_x")
-#
-#
-#
-# bcdata %>%
-#   mutate(value = if_else(value > great, great, value)) %>%
-#   ggplot() +
-#   ## great
-#   geom_col(aes(x = variable, y = great), fill = "grey10") +
-#   ## good
-#   geom_col(aes(x = variable, y = good), fill = "grey20") +
-#   ## bad
-#   geom_col(aes(x = variable, y = bad), fill = "grey") +
-#   ## present
-#   geom_col(aes(x = variable, y = value), fill = "black" ,
-#            width = 0.2) +
-#   scale_y_continuous(limits = c(0, NA)) +
-#   coord_flip() +
-#   facet_wrap(~variable, scales = "free_x") +
-#   theme(axis.text.y = element_text(size = 15, face = "bold"),
-#         axis.title.x = element_text(face = "bold", size = 10,
-#                                     margin = margin(t = 25, r = 0, b = 20, l = 0)),
-#         axis.text.x = element_text(face = "bold", size = 12),
-#         title = element_text(face = "bold"),
-#         plot.title = element_text(hjust = 0.5),
-#         plot.subtitle = element_text(hjust = 0.5, size = 8))
-#
-#
-#
-# bcdata %>%
-#   mutate(value = if_else(value > great, great, value)) %>%
-#   ggplot() +
-#   ## great
-#   geom_col(aes(x = 1, y = great), fill = "#dcdcdc") +
-#   ## good
-#   geom_col(aes(x = 1, y = good), fill = "#c0c0c0") +
-#   ## bad
-#   geom_col(aes(x = 1, y = bad), fill = "#696969") +  # grey20
-#   ## present
-#   geom_col(aes(x = 1, y = value), fill = "black" ,
-#            width = 0.2) +
-#   scale_y_continuous(limits = c(0, NA),
-#                      expand = c(0, 0)) +
-#   scale_x_continuous(expand = c(0, 0)) +
-#   coord_flip() +
-#   facet_wrap(~variable, scales = "free_x", ncol = 1) +
-#   theme(title = element_text(face = "bold"),
-#         plot.title = element_text(hjust = 0.5),
-#         plot.subtitle = element_text(hjust = 0.5, size = 8),
-#         panel.grid = element_blank(),
-#         axis.title.x = element_blank(),
-#         axis.text.x = element_text(face = "bold", size = 12),
-#         axis.title.y = element_blank(),
-#         axis.text.y = element_blank(),
-#         axis.ticks.y = element_blank(),
-#         strip.text = element_text(face = "bold", size = 14),
-#         strip.background = element_rect(fill = "white"))
-#
-# ## how to add legend
-# ## specifying df cols to cols in plot code >>>> preprocessing + rlang
+#' @title bulletchart
+#' @description FUNCTION_DESCRIPTION
+#' @param file_name PARAM_DESCRIPTION, Default: NULL
+#' @param sheet_name PARAM_DESCRIPTION, Default: 'Sheet1'
+#' @param dataframe PARAM_DESCRIPTION, Default: NULL
+#' @param indicator_name PARAM_DESCRIPTION, Default: 'variable'
+#' @param info PARAM_DESCRIPTION, Default: 'info'
+#' @param current PARAM_DESCRIPTION, Default: 'current'
+#' @param bad PARAM_DESCRIPTION, Default: 'bad'
+#' @param good PARAM_DESCRIPTION, Default: 'good'
+#' @param great PARAM_DESCRIPTION, Default: 'great'
+#' @param target PARAM_DESCRIPTION, Default: 'target'
+#' @param remove_no_targets PARAM_DESCRIPTION, Default: TRUE
+#' @param legend PARAM_DESCRIPTION, Default: FALSE
+#' @return OUTPUT_DESCRIPTION
+#' @details Stephen Few style bullet chart
+#' @rdname bulletchart
+#' @export
+
+bulletchart <- function(file_name = NULL, sheet_name = "Sheet1",
+                        dataframe = NULL,
+                        indicator_name = "variable",
+                        info = "info",
+                        current = "current",
+                        bad = "bad",
+                        good = "good",
+                        great = "great",
+                        target = "target",
+                        remove_no_targets = TRUE,
+                        legend = FALSE) {
+  ## Transform data
+  ammended_data <- field_calculator(file_name, sheet_name,
+                                    dataframe,
+                                    indicator_name, info,
+                                    current, bad, good, great,
+                                    target, remove_no_targets)
+
+  ## check for Target == 0 in all Targets
+  if(all(ammended_data$target == 0)) {
+    return(
+      "No Non-Zero Targets!"
+    )
+  }
+
+  ## fill colors
+  cols <- c(great = "#dcdcdc", good = "#c0c0c0", bad = "#696969",
+            current = "black")
+
+  ## PLOT
+  g <- ammended_data %>%
+    ggplot() +
+    ## great
+    geom_col(data = ammended_data %>% filter(allvals == "great"),
+             aes(x = 1, y = vals, fill = allvals)) +
+    ## good
+    geom_col(data = ammended_data %>% filter(allvals == "good"),
+             aes(x = 1, y = vals, fill = allvals)) +
+    ## bad
+    geom_col(data = ammended_data %>% filter(allvals == "bad"),
+             aes(x = 1, y = vals, fill = allvals)) +
+    coord_flip() +
+    facet_wrap(~indicator_name, scales = "free_x", ncol = 1) +
+    ## current
+    geom_col(data = ammended_data %>% filter(allvals == "current"),
+             aes(x = 1, y = vals), fill = "black" ,
+             width = 0.2) +
+    ## target
+    geom_segment(aes(x = 0.75, xend = 1.25,
+                     y = target, yend = target),
+                 color = "red", size = 2) +
+    scale_y_continuous(limits = c(0, NA),
+                       expand = c(0, 0)) +
+    scale_x_continuous(expand = c(0, 0)) +
+    scale_fill_manual(values = cols, name = "Values") +
+    theme(title = element_text(face = "bold"),
+          plot.title = element_text(hjust = 0.5),
+          plot.subtitle = element_text(hjust = 0.5, size = 8),
+          panel.grid = element_blank(),
+          axis.title.x = element_blank(),
+          axis.text.x = element_text(face = "bold", size = 12),
+          axis.title.y = element_blank(),
+          axis.text.y = element_blank(),
+          axis.ticks.y = element_blank(),
+          strip.text = element_text(face = "bold", size = 14),
+          strip.background = element_rect(fill = "white"))
+
+  if (legend == FALSE) {
+    g <- g + theme(legend.position = "none")
+    print(g)
+  }
+
+  print(g)
+}
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ R/internal2.R
@@ -0,0 +1,101 @@
+# internal functions ------------------------------------------------------
+
+#' @title extra_field_calculator
+#' @description internal function for calculating the extra fields needed for bullet charts
+#' @param file_name path of Excel file
+#' @param sheet_name specify which sheet in Excel file
+#' @param dataframe specify R dataframe input
+#' @param indicator_name specify the name of the column that has your indicator/KPI names
+#' @param info extra info for indicators (currency, percentage, symbol, etc.)
+#' @param current current value of indicator
+#' @param bad column with value for "bad"
+#' @param good column with value for "good"
+#' @param great column with value for "great", if "target" exceeds "great" then
+#' "great" will be increased to "target" value. If "current" value exceeds "great"
+#' then "current" value will be decreased to "great" value.
+#' @param target column with value for the "target"
+#' @param remove_no_targets Removes indicators without a "target" value specified, default: TRUE
+#' @details internal function for calculating the extra fields for the bullet chart
+#' @rdname field_calculator
+#' @importFrom dplyr mutate %>% case_when filter
+#' @importFrom testthat test_that expect_equal expect_true
+#' @importFrom readxl read_xlsx
+#' @importFrom lubridate year month
+#' @importFrom rlang enquo !!
+#' @importFrom glue glue
+
+field_calculator <- function(file_name = NULL, sheet_name = "Sheet1",
+                             dataframe = NULL,
+                             indicator_name = "variable",
+                             info = "info",
+                             current = "current",
+                             bad = "bad",
+                             good = "good",
+                             great = "great",
+                             target = "target",
+                             remove_no_targets = TRUE) {
+
+
+  ## Ensure both dataframe and file not provided
+  test_that("Only one dataset inputted: dataframe OR file_name - not both",
+            expect_true(is.null(file_name) | is.null(dataframe)))
+
+  ## Read in excel file or dataframe
+  if (!is.null(file_name)) {
+    ## Read in Excel file:
+    ammended_data <- read_xlsx(path = file_name, sheet = sheet_name)
+
+  } else if(!is.null(dataframe)) {
+    ## dataframe provided
+    ammended_data <- dataframe
+  }
+
+  ## If ammended_data is empty, break function and output empty chart
+  if (nrow(ammended_data) == 0){
+    return("No data available")
+  }
+
+  ## Assign field names to this dataset
+  ind <- enquo(indicator_name)
+  inf <- enquo(info)
+  cur <- enquo(current)
+  bad <- enquo(bad)
+  good <- enquo(good)
+  great <- enquo(great)
+  tar <- enquo(target)
+
+  ammended_data <- ammended_data %>%
+    select(indicator_name = !!ind,
+           current = !!cur,
+           bad = !!bad,
+           good = !!good,
+           great = !!great,
+           target = !!tar
+    )
+
+  ## filter NO Target indicators
+  if(remove_no_targets == TRUE) {
+    ammended_data <- ammended_data %>%
+      filter(!is.na(target), target != 0)
+  } else {
+    ammended_data
+  }
+
+  ammended_data <- ammended_data %>%
+  ## Protect against values greater than what's set at "great"
+    mutate(current = if_else(current > great, great, current)) %>%
+  ## if target value greater than "great", move "great" to "Target" value
+    mutate(great = if_else(target > great, target, great))
+
+  ## reshape
+  ammended_data <- ammended_data %>%
+    pivot_longer(-c(indicator_name, target),
+                 names_to = "allvals",
+                 values_to = "vals")
+
+  # Variale info text:
+  ammended_data <- ammended_data %>%
+    mutate(varinfo = glue("{indicator_name}: {info}"))
+
+  return(ammended_data)
+}
