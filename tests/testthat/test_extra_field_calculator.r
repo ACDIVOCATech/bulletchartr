a388253df8e1e952007e3afc526475f3c803783b
Ryo-N7 <ryonakagawara@gmail.com>
2020-03-02 19:40:47 +0900

    transition to PTnew, references ACDIVOCATech/AVDGleap#37

--- tests/testthat/test_extra_field_calculator.r
+++ tests/testthat/test_extra_field_calculator.r
@@ -1,7 +1,7 @@
 context("extra_field_calculator")
 
 #testthat::test_that("only one file type inputted",
-#                    expect_equal(extra_field_calculator(file_name = "data/Indicators_Targets.xlsx"),
+#                    expect_equal(extra_field_calculator(file_name = "data/Indicators_Targets_NEW.xlsx"),
 #                                 extra_field_calculator(file_name = "data/Indiacotrs_Targets.xlsx", dataframe = NULL)))
 
 
@@ -11,12 +11,12 @@ context("extra_field_calculator")
 
 
 # # ## Inputs
-# fileName = "Indicators_Targets.com"   # NOT excel file
-# fileName = here::here("inst/data/Indicators_Targets.xlsx")  # excel file
+# fileName = "Indicators_Targets_NEW.com"   # NOT excel file
+# fileName = here::here("inst/data/Indicators_Targets_NEW.xlsx")  # excel file
 # for_year = 2018 ## Specify Year the analysis represents
 # FY = TRUE       ## Is this a fiscal year? (as opposed to calendar year)
 # project_start_date <- "2016/03/01"   # as string! %Y/%D/%M
 #
 # ##
-# dataframe <- extra_field_calculator("data/Indicators_Targets.xlsx", for_year = 2018)
+# dataframe <- extra_field_calculator("data/Indicators_Targets_NEW.xlsx", for_year = 2018)
 # df <- extra_field_calculator(fileName, for_year = 2018)
